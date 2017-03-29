62576111ff68c082b078f63de90a87bfb334ca10
Scott McMurray <Scott@scotts-imac.local>
2017-03-28 21:44:14 -0600

    Initial Commit

---
+++ Puppy Love/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Puppy Love
+//
+//  Created by Scott McMurray on 3/28/17.
+//  Copyright © 2017 Scott McMurray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
